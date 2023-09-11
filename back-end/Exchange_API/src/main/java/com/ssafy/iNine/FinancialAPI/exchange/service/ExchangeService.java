f843ddbe9a294706590bc3b20105160f6fd27c35
김하영 <haha0691@koreatech.ac.kr>
2023-09-11 13:34:48 +0900

    feat: update exchange automatically

--- back-end/Exchange_API/src/main/java/com/ssafy/iNine/FinancialAPI/exchange/service/ExchangeService.java
+++ back-end/Exchange_API/src/main/java/com/ssafy/iNine/FinancialAPI/exchange/service/ExchangeService.java
@@ -1,7 +1,11 @@
 package com.ssafy.iNine.FinancialAPI.exchange.service;
 
+import com.ssafy.iNine.FinancialAPI.common.exception.CommonException;
+import com.ssafy.iNine.FinancialAPI.common.exception.ExceptionType;
+import com.ssafy.iNine.FinancialAPI.entity.Exchange;
 import com.ssafy.iNine.FinancialAPI.exchange.dto.Bank;
 import com.ssafy.iNine.FinancialAPI.exchange.dto.ExchangeDto;
+import com.ssafy.iNine.FinancialAPI.exchange.repository.ExchangeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONArray;
@@ -11,8 +15,11 @@ import org.json.simple.parser.ParseException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -29,15 +36,12 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class ExchangeService {
+    private final ExchangeRepository exchangeRepository;
     private static HttpURLConnection connection;
     private static BigDecimal defaultExchangeRate = BigDecimal.valueOf(1300);
+    
 
-    public List<List<ExchangeDto>> getExchangeList() throws IOException {
-        List<List<ExchangeDto>> exchangeList = getMyBank();
-        return exchangeList;
-    }
-
-    private List<List<ExchangeDto>> getMyBank() throws IOException {
+    public List<List<ExchangeDto>> getMyBank() throws IOException {
         List<List<ExchangeDto>> exchangeList = new ArrayList<>();
 //        List<String> codes = Arrays.asList("005", "020", "004", "088", "011", "003", "023");
         List<Bank> banks = Arrays.asList(Bank.values());
@@ -47,29 +51,20 @@ public class ExchangeService {
             String URL = "https://www.mibank.me/exchange/bank/index.php?search_code="+ banks.get(b).getCode();
             Document doc = Jsoup.connect(URL).get();
             Elements countryList = doc.select("tbody tr");
-    //        System.out.println(countryList);
             for(int i=0; i<countryList.size(); i++) {
                 Elements country = countryList.get(i).select("td");
                 ExchangeDto exchangeDto = new ExchangeDto();
                 exchangeDto.setBank(banks.get(b).getName());
-    //            System.out.println(country.get(1).text());
                 exchangeDto.setCountry(country.get(1).text());
                 if(country.get(2).text().equals("-")) continue;
-    //            System.out.println(country.get(2).text());
                 exchangeDto.setCashBuyPrice(new BigDecimal(country.get(2).text()));
-    //            System.out.println(country.get(3).text());
                 exchangeDto.setCashBuyRate(new BigDecimal(country.get(3).text().split("%")[0]));
-    //            System.out.println(country.get(4).text());
                 exchangeDto.setCashSellPrice(new BigDecimal(country.get(4).text()));
-    //            System.out.println(country.get(5).text());
                 exchangeDto.setCashSellRate(new BigDecimal(country.get(5).text().split("%")[0]));
                 if(!country.get(6).text().equals("-")) {
-    //                System.out.println(country.get(6).text());
                     exchangeDto.setTransferPrice(new BigDecimal(country.get(6).text()));
-    //                System.out.println(country.get(7).text());
                     exchangeDto.setTransferRate(new BigDecimal(country.get(7).text().split("%")[0]));
                 }
-    //            System.out.println(country.get(8).text());
                 exchangeDto.setPrice(new BigDecimal(country.get(8).text()));
                 bankList.add(exchangeDto);
             }
@@ -78,6 +73,78 @@ public class ExchangeService {
         return exchangeList;
     }
 
+    @Async
+//    @Scheduled(fixedDelay = 5000)
+    @Scheduled(fixedDelay = 300000)
+    @Transactional
+    public void updateExchange() throws IOException {
+        System.out.println("환율 업데이트");
+        List<Bank> banks = Arrays.asList(Bank.values());
+
+        for(int b=0; b<banks.size(); b++) {
+            String URL = "https://www.mibank.me/exchange/bank/index.php?search_code="+ banks.get(b).getCode();
+            Document doc = Jsoup.connect(URL).get();
+            Elements countryList = doc.select("tbody tr");
+            for(int i=0; i<countryList.size(); i++) {
+                Elements country = countryList.get(i).select("td");
+                Exchange exchange = exchangeRepository.findByBankAndCountry(banks.get(b).getName(), country.get(1).text())
+                        .orElseThrow(() -> new CommonException(ExceptionType.EXHANGE_NOT_FOUND));
+                if(!country.get(2).text().equals("-")) {
+                    exchange.setCashBuyPrice(new BigDecimal(country.get(2).text()));
+                    exchange.setCashBuyRate(new BigDecimal(country.get(3).text().split("%")[0]));
+                } else {
+                    exchange.setCashBuyPrice(null);
+                    exchange.setCashBuyRate(null);
+                }
+                if(!country.get(4).text().equals("-")) {
+                    exchange.setCashSellPrice(new BigDecimal(country.get(4).text()));
+                    exchange.setCashSellRate(new BigDecimal(country.get(5).text().split("%")[0]));
+                } else {
+                    exchange.setCashSellPrice(null);
+                    exchange.setCashSellRate(null);
+                }
+                if(!country.get(6).text().equals("-")) {
+                    exchange.setTransferPrice(new BigDecimal(country.get(6).text()));
+                    exchange.setTransferRate(new BigDecimal(country.get(7).text().split("%")[0]));
+                } else {
+                    exchange.setTransferPrice(null);
+                    exchange.setTransferRate(null);
+                }
+                exchange.setPrice(new BigDecimal(country.get(8).text()));
+            }
+        }
+    }
+
+    public void insertExchange() throws IOException {
+        List<Bank> banks = Arrays.asList(Bank.values());
+
+        for(int b=0; b<banks.size(); b++) {
+            List<ExchangeDto> bankList = new ArrayList<>();
+            String URL = "https://www.mibank.me/exchange/bank/index.php?search_code="+ banks.get(b).getCode();
+            Document doc = Jsoup.connect(URL).get();
+            Elements countryList = doc.select("tbody tr");
+            for(int i=0; i<countryList.size(); i++) {
+                Elements country = countryList.get(i).select("td");
+                Exchange exchange = new Exchange();
+                exchange.setBank(banks.get(b).getName());
+                exchange.setCountry(country.get(1).text());
+                if(!country.get(2).text().equals("-")) {
+                    exchange.setCashBuyPrice(new BigDecimal(country.get(2).text()));
+                    exchange.setCashBuyRate(new BigDecimal(country.get(3).text().split("%")[0]));
+                }
+                if(!country.get(4).text().equals("-")) {
+                    exchange.setCashSellPrice(new BigDecimal(country.get(4).text()));
+                    exchange.setCashSellRate(new BigDecimal(country.get(5).text().split("%")[0]));
+                }
+                if(!country.get(6).text().equals("-")) {
+                    exchange.setTransferPrice(new BigDecimal(country.get(6).text()));
+                    exchange.setTransferRate(new BigDecimal(country.get(7).text().split("%")[0]));
+                }
+                exchange.setPrice(new BigDecimal(country.get(8).text()));
+                exchangeRepository.save(exchange);
+            }
+        }
+    }
 
     // 한국수출입은행
     public List<ExchangeDto> getExchangeRateList() {
@@ -158,4 +225,16 @@ public class ExchangeService {
     }
 
 
+    public Map<String, Object> getExchangeList() {
+        Map<String, Object> map = new HashMap<>();
+
+        List<Bank> bankList = Arrays.asList(Bank.values());
+        List<String> banks = new ArrayList<>();
+        for(int i=0; i<bankList.size(); i++) banks.add(bankList.get(i).getName());
+        map.put("bank", banks);
+
+        List<Exchange> exchangeList = exchangeRepository.findAll();
+        map.put("list", exchangeList);
+        return map;
+    }
 }
