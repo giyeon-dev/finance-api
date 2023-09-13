b01b862002bba1e5d0f6110f61f3a47e4423a07c
홍유빈 <acrow0330@naver.com>
2023-09-13 17:58:30 +0900

    feat: oauth 설정

---
+++ back-end/oauth/src/main/java/com/ssafy/iNine/FinancialAPI/domain/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.ssafy.iNine.FinancialAPI.domain.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping("/login")
+    public String login() {
+        return "emailForm"; // login.html 또는 login.jsp와 같은 로그인 페이지 템플릿의 이름을 반환합니다.
+    }
+
+    @ResponseBody
+    @GetMapping("/auth")
+    public String authenticate() {
+        return "success"; // login.html 또는 login.jsp와 같은 로그인 페이지 템플릿의 이름을 반환합니다.
+    }
+}
