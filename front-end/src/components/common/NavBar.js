099028f7d9384c144ca784258cc6ac37eaa0d5dd
giyeonkwon013 <giyeonkwon013@gmail.com>
2023-09-22 11:27:27 +0900

    feat: navbar css 구현

--- front-end/src/components/common/NavBar.js
+++ front-end/src/components/common/NavBar.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
+import styles from "./NavBar.module.css";
+
+import { Link } from "react-router-dom";    
+
 const NavBar = () => {
     return (
-        <div>
-            <p>NavBar입니다. 여긴 지우시면됩니다</p>
+        <div className={styles.mainNav}>
+            <div className={styles.navLeft}>
+                <Link className={styles.navMenu} to = "/">Home</Link>
+                <Link className={styles.navMenu} to = "/serviceinfo">서비스 소개</Link>
+                <Link className={styles.navMenu} to = "/apidock">API문서</Link>  
+            </div>
+
+            <div className={styles.navRight}>
+                <Link className={styles.navMenu} to = "/login">로그인</Link>
+                <Link className={styles.navMenu}  to = "/signup">회원가입</Link>
+             </div>       
         </div>
     );
 };
