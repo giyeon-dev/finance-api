96501e3dfa9d032e8a870b10aace5218349ac4f3
강현곤 <shoot_1@naver.com>
2023-09-22 16:28:55 +0900

    feat: 로그인 기능추가

--- front-end/src/components/login/Login.js
+++ front-end/src/components/login/Login.js
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import styles from './Login.module.css';
 const Login = () => {
     return (
-        <div>
-            <p>Login입니다. 여긴 지우시면됩니다</p>
+        <div className={styles.loginBody}>
+            <div className={styles.loginContainer}>
+                <div className={styles.logoText}>S.F.O.API</div>
+                <input className={styles.loginInput} type="text" placeholder="아이디" />
+                <input className={styles.loginInput} type="password" placeholder="비밀번호" />
+                <button className={styles.loginBtn}>로그인</button>
+                <div className={styles.otherContainer}>
+                    <a className={`${styles.rightBorder} ${styles.otherBtn}`} href="#">
+                        <span>비밀번호 재설정</span>
+                    </a>
+                    <a className={`${styles.alignRight} ${styles.otherBtn}`} href="#">
+                        <span>회원가입</span>
+                    </a>
+                </div>
+            </div>
         </div>
     );
 };
