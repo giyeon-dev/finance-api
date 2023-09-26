dc9079d72c778b9d0943b03d6435bcbf8963c4da
강현곤 <shoot_1@naver.com>
2023-09-27 04:31:04 +0900

    로그인시 userInfo

--- front-end/src/components/login/Login.js
+++ front-end/src/components/login/Login.js
@@ -44,13 +44,13 @@ const Login = () => {
 			localStorage.setItem("access-token", res.data.data["access-token"]);
 			localStorage.setItem("refresh-token", res.data.data["refresh-token"]);
 
-			// 유저 데이터 redux에 저장
-			tokenHttp.get(`/docs/service/token`).then((response) => {
-				console.log(response);
-				localStorage.setItem("refresh-token", res.data.data["api-token"]);
-				dispatch(setUserInfo({ apiToken: response.data.data }));
-				console.log(userInfo);
-			});
+			// // 유저 데이터 redux에 저장
+			// tokenHttp.get(`/docs/service/token`).then((response) => {
+			// 	console.log(response);
+			// 	localStorage.setItem("refresh-token", res.data.data["api-token"]);
+			// 	dispatch(setUserInfo({ apiToken: response.data.data }));
+			// 	console.log(userInfo);
+			// });
 			navigate("/");
 			alert("로그인 성공");
 		} catch (error) {
