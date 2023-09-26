9add5faf6eac28eb3042205ad0da306bb979456d
강현곤 <shoot_1@naver.com>
2023-09-27 05:29:39 +0900

    fix: 로그인시 userInfo

--- front-end/src/components/mypage/Mypage.js
+++ front-end/src/components/mypage/Mypage.js
@@ -1,74 +1,75 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import styles from './Mypage.module.css';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./Mypage.module.css";
 
-import basicHttp from '../../api/basicHttp';
-import tokenHttp from '../../api/tokenHttp';
+import basicHttp from "../../api/basicHttp";
+import tokenHttp from "../../api/tokenHttp";
 
 const Mypage = () => {
-    // useState, useEffect
-    const [apiToken, setApiToken] = useState('');
-    const [redirectURL, setRedirectURL] = useState('');
-    useEffect(() => {
-        const getApiToken = async () => {
-            const res = await tokenHttp.get(`/docs/service/token`);
-            console.log(res);
-            console.log('api토큰 발급 성공');
+	// useState, useEffect
+	const [apiToken, setApiToken] = useState("");
+	const [redirectURL, setRedirectURL] = useState("");
+	useEffect(() => {
+		const getApiToken = async () => {
+			const res = await tokenHttp.get(`/docs/service/token`);
+			console.log(res);
+			console.log("api토큰 발급 성공");
 
-            localStorage.setItem('api-token', res.data.data);
-        };
+			localStorage.setItem("api-token", res.data.data);
+			setApiToken(res.data.data);
+		};
 
-        getApiToken();
-    }, []);
-    // input value 수정되면 set해주는 함수
-    const handleapiToken = (e) => {
-        setApiToken(e.target.value);
-    };
-    const handleRedirectURL = (e) => {
-        setRedirectURL(e.target.value);
-    };
+		getApiToken();
+	}, []);
+	// input value 수정되면 set해주는 함수
+	const handleapiToken = (e) => {
+		setApiToken(e.target.value);
+	};
+	const handleRedirectURL = (e) => {
+		setRedirectURL(e.target.value);
+	};
 
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 
-    async function onClickapiTokenRefreshBtn() {
-        try {
-            const res = await tokenHttp.get(`/docs/service/token`);
-            console.log(res);
-            console.log('api토큰 재발급 성공');
+	async function onClickapiTokenRefreshBtn() {
+		try {
+			const res = await tokenHttp.get(`/docs/service/token`);
+			console.log(res);
+			console.log("api토큰 재발급 성공");
 
-            localStorage.setItem('api-token', res.data.data);
-            alert('api토큰 재발급 성공');
-        } catch (error) {
-            console.error('api토큰 재발급  실패:', error);
-            alert('api토큰 재발급  실패');
-        }
-    }
+			localStorage.setItem("api-token", res.data.data);
+			alert("api토큰 재발급 성공");
+		} catch (error) {
+			console.error("api토큰 재발급  실패:", error);
+			alert("api토큰 재발급  실패");
+		}
+	}
 
-    return (
-        <div className={styles.mypageBody}>
-            <div className={styles.mypageContainer}>
-                <div className={styles.logoText}>마이페이지</div>
-                <input
-                    className={styles.apiTokenInput}
-                    value={apiToken}
-                    onChange={handleapiToken}
-                    type="text"
-                    placeholder="apiToken"
-                    disabled
-                />
-                <button className={styles.apiTokenRefreshBtn} onClick={onClickapiTokenRefreshBtn}>
-                    토큰 재발급
-                </button>
-                <input
-                    className={styles.inputBox}
-                    value={redirectURL}
-                    onChange={handleRedirectURL}
-                    type="text"
-                    placeholder=""
-                />
-            </div>
-        </div>
-    );
+	return (
+		<div className={styles.mypageBody}>
+			<div className={styles.mypageContainer}>
+				<div className={styles.logoText}>마이페이지</div>
+				<input
+					className={styles.apiTokenInput}
+					value={apiToken}
+					onChange={handleapiToken}
+					type="text"
+					placeholder="apiToken"
+					disabled
+				/>
+				<button className={styles.apiTokenRefreshBtn} onClick={onClickapiTokenRefreshBtn}>
+					토큰 재발급
+				</button>
+				<input
+					className={styles.inputBox}
+					value={redirectURL}
+					onChange={handleRedirectURL}
+					type="text"
+					placeholder=""
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default Mypage;
