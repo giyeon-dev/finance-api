325fdfd9ad7e7bf9e6e388d05972deae6924588c
강현곤 <shoot_1@naver.com>
2023-09-27 10:42:25 +0900

    feat: 토큰

--- front-end/src/api/tokenHttp.js
+++ front-end/src/api/tokenHttp.js
@@ -1,10 +1,10 @@
 //src/api/tokenHttp.js
-import axios from "axios";
-import jwt_decode from "jwt-decode";
-import dayjs from "dayjs";
-import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { logout } from "../redux/userInfo";
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import dayjs from 'dayjs';
+import { useNavigate, useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { logout } from '../redux/userInfo';
 
 // 토큰이 필요한 인증에 사용
 
@@ -12,44 +12,50 @@ const baseURL = process.env.REACT_APP_SERVER_URL;
 // const baseURL = "https://j9b309.p.ssafy.io";
 
 const tokenHttp = axios.create({
-	baseURL,
-	header: {
-		"Content-Type": "application/json",
-	},
+    baseURL,
+    header: {
+        'Content-Type': 'application/json',
+    },
 });
 
 // 요청 인터셉터 설정 (요청 보내기 전에 수행되는 함수)
 tokenHttp.interceptors.request.use(async (req) => {
-	const accessToken = localStorage.getItem("access-token");
-	if (!accessToken) {
-		console.log("token 이 존재하지 않습니다.");
-		throw new Error("expire token");
-	}
+    const accessToken = localStorage.getItem('access-token');
+    try {
+        if (!accessToken) {
+            console.log('token 이 존재하지 않습니다.');
+            throw new Error('expire token');
+        }
 
-	const user = jwt_decode(accessToken);
-	const isExpired = dayjs().diff(dayjs.unix(user.exp)) < 1;
+        const user = jwt_decode(accessToken);
+        const isExpired = dayjs().diff(dayjs.unix(user.exp)) < 1;
 
-	// access token 이 만료되지 않았다면 access-token 을 넣어 요청 실행
-	if (isExpired) {
-		req.headers["Authorization"] = `Bearer ${accessToken}`;
-		return req;
-	}
+        // access token 이 만료되지 않았다면 access-token 을 넣어 요청 실행
+        if (isExpired) {
+            req.headers['Authorization'] = `Bearer ${accessToken}`;
+            return req;
+        }
 
-	// 만료되었다면 강제 로그아웃
-	console.log("api/tokenHttp.js : access token 만료");
-	const dispatch = useDispatch();
-	dispatch(logout());
-	localStorage.removeItem("access-token");
-	localStorage.removeItem("refresh-token");
+        // 만료되었다면 강제 로그아웃
+        console.log('api/tokenHttp.js : access token 만료');
+        const dispatch = useDispatch();
+        dispatch(logout());
+        localStorage.removeItem('access-token');
+        localStorage.removeItem('refresh-token');
 
-	// 여기에서 로그인 페이지로 리디렉션을 수행할 수 있습니다.
-	// 예를 들어, React Router의 navigate 또는 history.push 등을 사용하여 로그인 페이지로 이동할 수 있습니다.
-	// 예시: history.push('/login');
-	const navigate = useNavigate();
-	navigate("/login");
+        // 여기에서 로그인 페이지로 리디렉션을 수행할 수 있습니다.
+        // 예를 들어, React Router의 navigate 또는 history.push 등을 사용하여 로그인 페이지로 이동할 수 있습니다.
+        // 예시: history.push('/login');
 
-	// 만료된 토큰으로 인한 오류를 던집니다.
-	throw new Error("expire refresh-token");
+        // 만료된 토큰으로 인한 오류를 던집니다.
+        throw new Error('expire refresh-token');
+    } catch (error) {
+        if (error.message === 'expire token' || error.message === 'expire refresh-token') {
+            // 로그아웃 또는 로그인 페이지로 이동하는 대신 상태를 변경합니다.
+            const navigate = useNavigate();
+            navigate('/login');
+        }
+    }
 });
 
 export default tokenHttp;
