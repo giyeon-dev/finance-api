06f44e0b295433b4ffb365bc8ec6118feae17e04
강현곤 <shoot_1@naver.com>
2023-10-04 12:43:27 +0900

    feat: 예시페이지

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,33 +1,36 @@
-import "./App.css";
-import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import './App.css';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 
 // 메인페이지
-import Home from "./components/main/Home";
+import Home from './components/main/Home';
 // 회원가입
-import Signup from "./components/signup/Signup";
+import Signup from './components/signup/Signup';
 // 로그인
-import Login from "./components/login/Login";
+import Login from './components/login/Login';
 // 마이페이지
-import Mypage from "./components/mypage/Mypage";
+import Mypage from './components/mypage/Mypage';
 // 서비스 소개
-import ServiceInfo from "./components/serviceinfo/ServiceInfo";
-import APIDocs from "./components/apidocs/APIDocs";
+import ServiceInfo from './components/serviceinfo/ServiceInfo';
+import APIDocs from './components/apidocs/APIDocs';
 // API문서
+// 예시 페이지
+import ExExchange from './components/examplepage/ExExchange';
 
 function App() {
-	return (
-		<div className="App">
-			<Routes>
-				<Route path="" element={<Home />} />
-				<Route path="/signup" element={<Signup />}></Route>
-				<Route path="/login" element={<Login />}></Route>
-				<Route path="/mypage" element={<Mypage />}></Route>
-				<Route path="/serviceinfo" element={<ServiceInfo />}></Route>
-				<Route path="/apidock" element={<APIDocs />} />
-				<Route path="/apidock/:api_docs_id" element={<APIDocs />}></Route>
-			</Routes>
-		</div>
-	);
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="" element={<Home />} />
+                <Route path="/signup" element={<Signup />}></Route>
+                <Route path="/login" element={<Login />}></Route>
+                <Route path="/mypage" element={<Mypage />}></Route>
+                <Route path="/serviceinfo" element={<ServiceInfo />}></Route>
+                <Route path="/apidock" element={<APIDocs />} />
+                <Route path="/apidock/:api_docs_id" element={<APIDocs />}></Route>
+                <Route path="/exexchange" element={<ExExchange />}></Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ front-end/src/components/examplepage/ExExchange.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './ExExchange.module.css';
+
+const ExExchange = () => {
+    return (
+        <div className={styles.loginBody}>
+            <div className={styles.loginContainer}>안녕하세요 ExExchange 입니다.</div>
+        </div>
+    );
+};
+
+export default ExExchange;
