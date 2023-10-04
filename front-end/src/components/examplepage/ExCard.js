0f2884b3a9d48b06f2c6cd33fd91edc45c2f5877
강현곤 <shoot_1@naver.com>
2023-10-04 16:30:07 +0900

    feat: 예시페이지

--- front-end/src/components/examplepage/ExCard.js
+++ front-end/src/components/examplepage/ExCard.js
@@ -9,16 +9,12 @@ import basicHttp from '../../api/basicHttp';
 import tokenHttp from '../../api/tokenHttp';
 
 const ExCard = () => {
-    const [exchangeAllList, setExchangeAllList] = useState([]);
-
     useEffect(() => {
         const getExchangeAll = async () => {
             try {
                 const res = await basicHttp.get(`/api/exchange`);
                 console.log(res.data.data.list);
                 console.log(res.data.data.list[2].price);
-
-                setExchangeAllList(res.data.data.list);
             } catch (error) {}
         };
         getExchangeAll();
@@ -34,7 +30,7 @@ const ExCard = () => {
                 <div className={styles.link}>
                     <a
                         href="https://j9b309.p.ssafy.io
-                        /oauth/authorize?response_type=code&client_id=5caa02bf-f494-4c83-a7b5-7d7a1591ba2a&redirect_uri=https://j9b309.p.ssafy.io/excardcontent&scope=read"
+/oauth/authorize?response_type=code&client_id=5caa02bf-f494-4c83-a7b5-7d7a1591ba2a&redirect_uri=https://j9b309.p.ssafy.io/excardcontent&scope=read"
                     >
                         내 카드내역 불러오기
                     </a>
