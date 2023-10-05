f352cf82fbbcfe12b14dfaaab10c72bdc59a51b4
giyeonkwon013 <giyeonkwon013@gmail.com>
2023-10-05 15:05:57 +0900

    fix: excardcontent

--- front-end/src/components/examplepage/ExCardContent.js
+++ front-end/src/components/examplepage/ExCardContent.js
@@ -10,14 +10,14 @@ const ExCardContent = () => {
 
   const [cardList, setCardList] = useState([]);
   const [selectedCardId, setSelectedCardId] = useState(null);
-  const [cardTransactions, setCardTransactions] = useState([]);
-  const [accessToken, setAccessToken] = useState(null); // 액세스 토큰 상태 추가
+  const [approvedList, setApprovedList] = useState([]);
 
   useEffect(() => {
     const getCardContent = async () => {
       try {
         const res = await basicHttp.get(`/oauth/access-token?code=${code}`);
-        setAccessToken(res.data.access_token); // 액세스 토큰을 상태로 저장
+        console.log(res);
+
         const res2 = await basicHttp.get(
           `https://j9b309.p.ssafy.io/api/cards?orgCode=exampleOrgcode&limit=10`,
           {
@@ -27,22 +27,26 @@ const ExCardContent = () => {
           }
         );
         setCardList(res2.data.data.cardList);
-      } catch (error) {}
+      } catch (error) {
+        console.log(error);
+      }
     };
     getCardContent();
-  }, [code]); // 코드가 변경될 때마다 액세스 토큰을 다시 가져오도록 설정
+  }, []);
 
   const handleCardClick = async (cardId) => {
     try {
-      const res = await tokenHttp.get(
+      const res = await basicHttp.get(`/oauth/access-token?code=${code}`);
+
+      const res2 = await tokenHttp.get(
         `https://j9b309.p.ssafy.io/api/cards/transaction?cardId=${cardId}&orgCode=exampleOrgcod&fromDate=2021-01-10&toDate=2024-09-21`,
         {
           headers: {
-            Authorization: `Bearer ${accessToken}`, // 저장된 액세스 토큰을 사용
+            Authorization: `Bearer ${res.data.access_token}`,
           },
         }
       );
-      setCardTransactions(res.data);
+      setApprovedList(res2.data.data.approvedList);
       setSelectedCardId(cardId);
     } catch (error) {}
   };
@@ -52,7 +56,6 @@ const ExCardContent = () => {
     require("../../assets/img/KB2.PNG"),
     require("../../assets/img/KB3.PNG"),
     require("../../assets/img/HD1.PNG"),
-    
   ];
 
   return (
@@ -68,9 +71,14 @@ const ExCardContent = () => {
             className={`${styles.chartBox} ${styles.chatBox1}`}
             onClick={() => handleCardClick(card.cardId)}
           >
-            <div className={`${styles.chartNumber} ${styles.chartNumber1}`}></div>
+            <div
+              className={`${styles.chartNumber} ${styles.chartNumber1}`}
+            ></div>
             <div className={`${styles.chartCover} ${styles.chartCover1}`}>
-              <img src={cardImages[Math.floor(Math.random() * cardImages.length)]} alt={card.cardName} />
+              <img
+                src={cardImages[Math.floor(Math.random() * cardImages.length)]}
+                alt={card.cardName}
+              />
             </div>
             <div className={`${styles.chartName} ${styles.chartName1}`}>
               <span>{card.cardName}</span>
@@ -87,9 +95,9 @@ const ExCardContent = () => {
           <div className={styles.transactionContainer}>
             <h2>선택한 카드의 거래 내역</h2>
             <ul>
-              {cardTransactions.map((transaction, index) => (
+              {approvedList.map((transaction, index) => (
                 <li key={index}>
-                  거래 내역: {transaction.approvedList}
+                  거래 내역: {transaction}
                   {/* 거래 내역의 다른 정보를 표시하려면 여기에 추가 */}
                 </li>
               ))}
